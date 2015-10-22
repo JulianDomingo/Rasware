55e64f1a977da45409586234cd0da45db8fb3323
Jose Camacho <josecam10596@gmail.com>
2015-10-22 17:44:02 -0500

    Moving Checpoint

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,24 +1,30 @@
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
+#include<RASLib/inc/servo.h>
 
 // Blink the LED to show we're on
 tBoolean blink_on = true;
 
+// Change these lines to set LED's to blink 
 void blink(void) {
     SetPin(PIN_F3, blink_on);
     blink_on = !blink_on;
 }
 
+tServo *servo1 = InitializeServo(pin);
+tServo *servo2 = InitializeServo(pin);
 
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
     CallEvery(blink, 0, 0.5);
-    
+   
     while (1) {
         // Runtime code can go here
-        Printf("Hello World!\n");
+	SetServo(servo1, .75f);
+	SetServo(servo2, .75f);
+	Printf("Hello World!\n");
         
     }
 }
