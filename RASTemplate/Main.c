5b65a923ce3d0efb4ea52ea4a3159c242cdbb63d
Jose Camacho <josecam10596@gmail.com>
2015-11-06 18:30:31 -0600

    Line sensor

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,7 +1,8 @@
-#include <RASLib/inc/common.h>
-#include <RASLib/inc/gpio.h>
-#include <RASLib/inc/time.h>
+#include<RASLib/inc/common.h>
+#include<RASLib/inc/gpio.h>
+#include<RASLib/inc/time.h>
 #include<RASLib/inc/servo.h>
+#include<RASLib/inc/linesensor.h>
 
 // Blink the LED to show we're on
 tBoolean blink_on = true;
@@ -12,19 +13,31 @@ void blink(void) {
     blink_on = !blink_on;
 }
 
-tServo *servo1;
-servo1 = InitializeServo(PIN_B2);
-tServo *servo2;
-servo2 = InitializeServo(PIN_F0);
-
 // The 'main' function is the entry point of the program
 int main(void) {
     // Initialization code can go here
     CallEvery(blink, 0, 0.5);
-   
+tServo *servo1;
+servo1 = InitializeServo(PIN_B2);
+tServo *servo2;
+servo2 = InitializeServo(PIN_F0);
+tLinesensor *gls;
+gls = InitializeGPIOLinesesnor(
+	PIN_B5,
+	PIN_D0,
+	PIN_D1,
+	PIN_D2,
+	PIN_D3,
+	PIN_D3,
+	PIN_E0,
+	PIN_C6,
+	PIN_C7
+	);
+	float line[8];
     while (1) {
         // Runtime code can go here
-	SetServo(servo1, .75f);
+	LineSensorReadArray(gls, line);
+	SetServo(servo1, .25f);
 	SetServo(servo2, .75f);
 	Printf("Hello World!\n");
         
