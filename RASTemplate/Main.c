c17fcd15cc18ed451144b74cae7cfa6d2d31910e
Jose Camacho <josecam10596@gmail.com>
2015-11-08 16:39:33 -0600

    Touch ups on code form

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -17,12 +17,15 @@ void blink(void) {
 int main(void) {
     // Initialization code can go here
     CallEvery(blink, 0, 0.5);
-tServo *servo1;
-servo1 = InitializeServo(PIN_B2);
-tServo *servo2;
-servo2 = InitializeServo(PIN_F0);
-tLinesensor *gls;
-gls = InitializeGPIOLinesesnor(
+// Name servo1 and 2 correlating to left/right.
+    tServo *servo1;
+    servo1 = InitializeServo(PIN_B2);
+    tServo *servo2;
+    servo2 = InitializeServo(PIN_F0);
+    tservo *gateservo;
+    gateservo = InitializeServo(PIN_XX);
+    tLinesensor *gls;
+    gls = InitializeGPIOLinesesnor(
 	PIN_B5,
 	PIN_D0,
 	PIN_D1,
@@ -37,13 +40,11 @@ float line[8];
     while (1) {
         // Runtime code can go here
 	LineSensorReadArray(gls, line);
-	SetServo(servo1, .25f);
-	SetServo(servo2, .75f);
 	Printf("Hello World!\n");
 	float left, right;
 	float abs difference;
-	left = line[0]+line[1]+line[2]+line[3]+line[4];
-	right =line[3]+line[4]+line[5]+line[6]+line[7];
+	left = line[0]+line[1]+line[2]+line[3];
+	right =line[4]+line[5]+line[6]+line[7];
 	difference = left-right;
 
 //If sensors detect line on left side, turn gradually right to align.
@@ -53,11 +54,16 @@ float line[8];
 	SetServo(servo1, .25f);
 	SetServo(servo2, .50f);
 }
-    if(right>1){
+   else if (right>1){
 	SetServo(servo1, .50f);
 	SetServo(servo2, .25f);
 }
-	else if(difference<1){
+   else if(difference<1){
 	SetServo(servo1, .25f);
 	SetServo(servo2, .75f);
+}
+   else {
+	SetServo(servo1, .25f);
+	SetServo(servo2, .75f);
+}
 }
